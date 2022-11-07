ecef049cc58260ff58822c7af128d3427d6d8dac
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-07 16:48:37 +0700

    Add project files.

---
+++ Singly_Linked_List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_Linked_List
+{
+    class Node
+    {
+        //each node consist of the information part and link to the next node
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
+    class List
+    {
+
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
