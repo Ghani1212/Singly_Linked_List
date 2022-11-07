99e753533dcffbfbd14bfc1d73bf6268d18dd4c8
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-07 17:37:41 +0700

    membuat list

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -117,6 +117,19 @@ namespace Singly_Linked_List
     {
         static void Main(string[] args)
         {
+            List obj = new List();
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1 Add a record to the list ");
+                    Console.WriteLine("2 Delete a record from the list ");
+                    Console.WriteLine("3 View all the records in the list ");
+                    Console.WriteLine("4 Search for a record in the list ");
+                    Console.WriteLine("5 EXIT ");
+                    
+            }
         }
     }
 }
